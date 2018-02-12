ffdac408465b650c8ca82e2c31b2d906ae45f4a8
Melissa414 <Missy41491@gmail.com>
2018-02-12 15:33:33 -0500

    added summaries

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -10,7 +10,12 @@ class App extends Component {
           <h1 className="App-title"><code>Hello World!</code></h1>
         </header>
 
-          <p className="summary">I am a front-end developer with a focus on building robust applications.</p>
+          <h5 className="summary">I am a front-end developer with a focus on building robust applications.</h5>
+          <p className= "workEthic">I feel having a strong work ethic is part of an individual's personal values. It's based in a belief in the value of work for its own sake and not just for its rewards. 
+          Being honest, having a positive attitude, taking initiative, caring about coworkers, doing more than expected, being reliable and responsible, learning new skills and being a team player are traits I strive to achieve.</p>
+          <p className= "teamWork">The skills I have acquired from successful group work has helped me for future careers. 
+          Working effectively, planning for the different stages of group activity. Time spent discussing how your group will work together will be invaluable in helping to create a constructive working team. The effective organization, communication and trouble shooting strategies described in this guide will help your group work productively to complete its task.</p>
+          <p className="loyalty">Working for my current employer has shown me great sense of longevity, dedication, and commitment. I have continued my education, by going to Rutgers Coding Bootcamp and through constant self-teaching.</p>
       </div>
     );
   }
